c5329dd52effd9a6a418555e1fd4f1cbaf165646
Aya22-g <moon22aikou@gmail.com>
2025-07-15 17:51:06 +0600

    Revert "Add files via upload"

--- java/com/example/controller/VacancyController.java
+++
@@ -1,9 +0,0 @@
-package com.example.controller;
-
-public class VacancyController {
-    private final VacancyService vacancyService;
-
-    public ResponseEntity<Vacancy> createVacancy(@RequestBody Vacancy vacancy) {
-        return ResponseEntity.ok(vacancyService.createVacancy(vacancy));
-    }
-}
